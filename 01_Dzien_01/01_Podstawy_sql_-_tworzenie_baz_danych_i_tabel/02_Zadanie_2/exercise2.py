d949fb877d2f2bf2017516bfce06197b07afd662
jgrynczewski <jgrynczewski@gmail.com>
2024-01-24 12:10:51 +0100

    zadanie 2 (tworzenie baz danych i tabel)

--- 01_Dzien_01/01_Podstawy_sql_-_tworzenie_baz_danych_i_tabel/02_Zadanie_2/exercise2.py
+++ 01_Dzien_01/01_Podstawy_sql_-_tworzenie_baz_danych_i_tabel/02_Zadanie_2/exercise2.py
@@ -1,1 +1,16 @@
-sql = "sql code should be placed here"
+sql1 = """
+CREATE TABLE products (
+     id SERIAL,
+     name VARCHAR(256),
+     description TEXT,
+     price DECIMAL(5,2)
+);
+"""
+
+sql2 = """
+CREATE TABLE orders (id SERIAL, description TEXT);
+"""
+
+sql3 = """
+CREATE TABLE clients (id SERIAL, name VARCHAR(256), surname VARCHAR(256));
+"""
