0aaa6ad23ecb033c3dcf5a1f9971758c02018688
jgrynczewski <jgrynczewski@gmail.com>
2024-01-24 12:37:18 +0100

    zadanie 4 (tworzenie baz danych i tabel)

--- 01_Dzien_01/01_Podstawy_sql_-_tworzenie_baz_danych_i_tabel/04_Zadanie_4/exercise4.py
+++ 01_Dzien_01/01_Podstawy_sql_-_tworzenie_baz_danych_i_tabel/04_Zadanie_4/exercise4.py
@@ -1,1 +1,15 @@
-sql = "sql code should be placed here"
+sql = """
+CREATE TABLE cinemas (id SERIAL PRIMARY KEY, name VARCHAR(256), address VARCHAR(256));
+"""
+
+sql2 = """
+CREATE TABLE movies (id SERIAL PRIMARY KEY, name VARCHAR(256), description VARCHAR(256), rating INTEGER);
+"""
+
+sql3 = """
+CREATE TABLE tickets (id SERIAL PRIMARY KEY, quantity INTEGER, price DECIMAL);
+"""
+
+sql4 = """
+CREATE TABLE payments (id SERIAL PRIMARY KEY, type VARCHAR(256), date DATE);
+"""
