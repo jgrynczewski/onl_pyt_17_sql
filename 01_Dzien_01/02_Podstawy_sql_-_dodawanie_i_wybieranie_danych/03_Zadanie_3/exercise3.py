90b23af2cc19f6ced23daa8c14e7309dd9327481
jgrynczewski <jgrynczewski@gmail.com>
2024-01-24 15:15:43 +0100

    zadanie 3 (dodawanie i wybieranie danych)

--- 01_Dzien_01/02_Podstawy_sql_-_dodawanie_i_wybieranie_danych/03_Zadanie_3/exercise3.py
+++ 01_Dzien_01/02_Podstawy_sql_-_dodawanie_i_wybieranie_danych/03_Zadanie_3/exercise3.py
@@ -1,6 +1,18 @@
-add_movies = "Write the query here!"
-add_tickets = "Write the query here!"
+add_movies = """
+INSERT INTO movies (name) VALUES ('Wall Street'), ('Witness'), ('Startreck'), ('Deep Space Nine');
+"""
+add_tickets = """
+INSERT INTO tickets (quantity, price) VALUES (1, 10), (2, 30), (4, 15), (5, 25);
+"""
 
-get_movies = "Write the query here!"
-get_tickets_1 = "Write the query here!"
-get_tickets_2 = "Write the query here!"
+get_movies = """
+SELECT name FROM movies WHERE 'W%';
+"""
+
+get_tickets_1 = """
+SELECT * FROM tickets WHERE price>15.30;
+"""
+
+get_tickets_2 = """
+SELECT * FROM tickets WHERE quantity>3;
+"""
