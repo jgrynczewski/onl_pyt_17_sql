d9a68a897b9684ae319ad7fa32381d4c2822208f
jgrynczewski <jgrynczewski@gmail.com>
2024-01-24 14:05:19 +0100

    zadanie 1 (dodawanie i wybieranie danych)

--- 01_Dzien_01/02_Podstawy_sql_-_dodawanie_i_wybieranie_danych/01_Zadanie_1/exercise1.py
+++ 01_Dzien_01/02_Podstawy_sql_-_dodawanie_i_wybieranie_danych/01_Zadanie_1/exercise1.py
@@ -1,3 +1,21 @@
-add_products_sql = "Write the query here!"
-add_orders_sql = "Write the query here!"
-add_clients_sql = "Write the query here!"
+add_products_sql = """
+INSERT INTO products(name, description, price) VALUES
+    ('lizak', 'truskawkowy', 10),
+    ('bmv', 'czarne', 999),
+    ('rower', 'bmx', 100)
+;
+"""
+
+add_orders_sql = """
+INSERT INTO orders(description) VALUES ('pierwsze'), ('drugie'), ('trzecie');
+"""
+
+add_clients_sql = """
+INSERT INTO clients(name, surname) VALUES ('Donald', 'Duck');
+INSERT INTO clients(name, surname) VALUES ('Micky', 'Mouse');
+INSERT INTO clients(name, surname) VALUES ('Sknerus', 'McKwacz');
+INSERT INTO clients(name, surname) VALUES
+    ('John', 'Doe'),
+    ('Jane', 'Doe')
+;
+"""
