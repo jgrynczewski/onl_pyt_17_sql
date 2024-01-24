1dac46fef351b49ff4a7ec874eaf6f56a4ec1f5c
jgrynczewski <jgrynczewski@gmail.com>
2024-01-24 15:15:49 +0100

    zadanie 4 (dodawanie i wybieranie danych)

--- 01_Dzien_01/02_Podstawy_sql_-_dodawanie_i_wybieranie_danych/04_Zadanie_4/exercise4.py
+++ 01_Dzien_01/02_Podstawy_sql_-_dodawanie_i_wybieranie_danych/04_Zadanie_4/exercise4.py
@@ -1,2 +1,12 @@
-add_cinemas = "Write the query here!"
-add_payments = "Write the query here!"
+add_cinemas = """
+    INSERT INTO cinemas(name, address) VALUES
+    ('Cinema City', 'Ostrobramska'),
+    ('Multikino', 'Aleja KEN'),
+    ('Femina', 'Aleja Soldiarnosci');
+"""
+add_payments = """
+    INSERT INTO payments(type, date) VALUES
+    ('cash', '12-04-2021'),
+    ('checks', '21-05-2022'),
+    ('credit card', '16-06-2022');
+"""
