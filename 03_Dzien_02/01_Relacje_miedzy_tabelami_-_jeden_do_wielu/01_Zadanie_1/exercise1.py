095c99b16559122bb4a8344a52812be416a2d3ab
jgrynczewski <jgrynczewski@gmail.com>
2024-01-25 11:55:01 +0100

    zadanie 1 (Relacje miedzy tabelami - jeden do wielu)

--- 03_Dzien_02/01_Relacje_miedzy_tabelami_-_jeden_do_wielu/01_Zadanie_1/exercise1.py
+++ 03_Dzien_02/01_Relacje_miedzy_tabelami_-_jeden_do_wielu/01_Zadanie_1/exercise1.py
@@ -1,3 +1,19 @@
-create_opinions_table = "Write the query here!"
-add_opinions = "Write the query here!"
-select_opinions = "Write the query here!"
+create_opinions_table = """
+CREATE TABLE opinions(
+    id SERIAL PRIMARY KEY, 
+    description TEXT, 
+    product_id INT, 
+    FOREIGN KEY(product_id) REFERENCES products(id)
+);
+"""
+
+add_opinions = """
+INSERT INTO opinions(description, product_id) VALUES 
+    ('Super!!', 2), 
+    ('Tak sobie', 2), 
+    ('Szybki!', 3);
+"""
+
+select_opinions = """
+select * from opinions where product_id=2;
+"""
