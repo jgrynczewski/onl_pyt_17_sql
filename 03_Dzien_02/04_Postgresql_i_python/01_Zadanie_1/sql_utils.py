e34c496a8c6a17c0041b66c058b640f3b7bde532
jgrynczewski <jgrynczewski@gmail.com>
2024-01-25 15:48:46 +0100

    Zadanie 1 (Postgresql i python)

--- 03_Dzien_02/04_Postgresql_i_python/01_Zadanie_1/sql_utils.py
+++ 03_Dzien_02/04_Postgresql_i_python/01_Zadanie_1/sql_utils.py
@@ -1,6 +1,8 @@
+from psycopg2 import connect, ProgrammingError
+
 USER = "postgres"
 HOST = "localhost"
-PASSWORD = "coderslab"
+PASSWORD = "postgres"
 
 
 def execute_sql(sql_code, db):
@@ -14,4 +16,39 @@ def execute_sql(sql_code, db):
     :return: data from psycobg2 cursor as a list (can be None) if nothing to fetch.
     """
     # Place exercise 1 solution here.
-    pass
+
+    # połączenie z bazą
+    cnx = connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        database=db
+    )
+
+    # tworzymy kursor
+    with cnx.cursor() as cursor:
+        # zapytanie
+        cursor.execute(sql_code)
+        # commit
+        cnx.commit()
+
+        try:
+            # fetchujemy (dla SELECT)
+            res = cursor.fetchall()
+        except ProgrammingError:
+            # jeżeli nic nie było do sfechowania (bo zapytanie INSERT, UPDATE lub DELETE)
+            res = None
+
+    # Zamykamy połączenie z bazą
+    cnx.close()
+
+    return res
+
+
+result = execute_sql(
+    sql_code="SELECT * FROM opinions;",
+    # sql_code="INSERT INTO opinions(description, product_id) VALUES ('Taki sobie', 2);",
+    db='exercises_db'
+)
+
+print(result)
---
+++ 03_Dzien_02/04_Postgresql_i_python/intro.py
@@ -0,0 +1,30 @@
+from psycopg2 import connect, ProgrammingError
+
+# query = "DELETE FROM opinions where id=2;"
+query = "SELECT * FROM opinions;"
+
+# połączenie z bazą
+cnx = connect(
+    user="postgres",
+    password="postgres",
+    database='exercises_db'
+)
+
+# tworzymy kursor
+with cnx.cursor() as cursor:
+    # zapytanie
+    cursor.execute(query)
+    # commit
+    cnx.commit()
+
+    try:
+        # fetchujemy (dla SELECT)
+        res = cursor.fetchall()
+    except ProgrammingError:
+        # jeżeli nic nie było do sfechowania (bo zapytanie INSERT, UPDATE lub DELETE)
+        res = None
+
+# Zamykamy połączenie z bazą
+cnx.close()
+
+print(res)
