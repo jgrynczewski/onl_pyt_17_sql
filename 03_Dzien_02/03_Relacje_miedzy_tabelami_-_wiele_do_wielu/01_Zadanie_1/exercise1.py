f00691fb539f8ecb77fa1f085b1b5a3714cb9e4c
jgrynczewski <jgrynczewski@gmail.com>
2024-01-25 14:18:09 +0100

    Zad 1 (Relacje miedzy tabelami - wiele do wielu)

--- 03_Dzien_02/03_Relacje_miedzy_tabelami_-_wiele_do_wielu/01_Zadanie_1/exercise1.py
+++ 03_Dzien_02/03_Relacje_miedzy_tabelami_-_wiele_do_wielu/01_Zadanie_1/exercise1.py
@@ -1,4 +1,25 @@
-create_products_orders = "Write the query here!"
-add_product_order_1 = "Write the query here!"
-add_product_order_2 = "Write the query here!"
-add_product_order_3 = "Write the query here!"
+create_products_orders = """
+CREATE TABLE products_orders(
+    id SERIAL PRIMARY KEY, 
+    products_id INT, 
+    orders_id INT, 
+    FOREIGN KEY(products_id) REFERENCES products(id), 
+    FOREIGN KEY(orders_id) REFERENCES orders(id)
+);
+"""
+
+add_product_order_1 = """
+INSERT INTO products_orders(products_id, orders_id) VALUES (2,1);
+"""
+
+add_product_order_2 = """
+INSERT INTO products_orders(products_id, orders_id) VALUES (2,3);
+"""
+
+add_product_order_3 = """
+INSERT INTO products_orders(products_id, orders_id) VALUES (3,2);
+"""
+
+add_product_order_4 = """
+INSERT INTO products_orders(products_id, orders_id) VALUES (3,3);
+"""
