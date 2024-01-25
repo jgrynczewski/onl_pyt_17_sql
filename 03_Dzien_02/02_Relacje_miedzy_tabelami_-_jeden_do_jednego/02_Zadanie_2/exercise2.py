7dcf4f02e35cec791030f7c6dfe54f1dcfc75a98
jgrynczewski <jgrynczewski@gmail.com>
2024-01-25 11:25:56 +0100

    zadanie 2 (Relacje miedzy tabelami - jeden do jednego)

--- 03_Dzien_02/02_Relacje_miedzy_tabelami_-_jeden_do_jednego/02_Zadanie_2/exercise2.py
+++ 03_Dzien_02/02_Relacje_miedzy_tabelami_-_jeden_do_jednego/02_Zadanie_2/exercise2.py
@@ -1,1 +1,9 @@
-create_table_payments = "Write the query here!"
+create_table_payments = """
+CREATE TABLE payments(
+    id SERIAL PRIMARY KEY, 
+    type VARCHAR(256), 
+    date DATE,
+    ticket_id INT, 
+    FOREIGN KEY(ticket_id) REFERENCES tickets(id)
+);
+"""
