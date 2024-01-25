0717cdcddaeb06eb911f4bc6250091b2ae6b7262
jgrynczewski <jgrynczewski@gmail.com>
2024-01-25 12:15:53 +0100

    zadanie 2 (Relacje miedzy tabelami - jeden do wielu)

--- 03_Dzien_02/01_Relacje_miedzy_tabelami_-_jeden_do_wielu/02_Zadanie_2/exercise2.py
+++ 03_Dzien_02/01_Relacje_miedzy_tabelami_-_jeden_do_wielu/02_Zadanie_2/exercise2.py
@@ -1,2 +1,14 @@
-create_comments_table = "Write the query here!"
-add_comments = "Write the query here!"
+create_comments_table = """
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY, 
+    content TEXT, 
+    movie_id INT, 
+    FOREIGN KEY(movie_id) REFERENCES movies(id));
+"""
+
+add_comments = """
+INSERT INTO comments (content, movie_id) VALUES 
+    ('super', 3), 
+    ('fajne', 3), 
+    ('ok', 2);
+"""
