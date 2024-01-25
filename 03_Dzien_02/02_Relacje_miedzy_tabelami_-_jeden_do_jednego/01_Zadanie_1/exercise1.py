66c6b0cabf82cc60c7393f9ac423963e7edf8734
jgrynczewski <jgrynczewski@gmail.com>
2024-01-25 11:05:41 +0100

    zadanie 1 (Relacje miedzy tabelami - jeden do jednego)

--- 03_Dzien_02/02_Relacje_miedzy_tabelami_-_jeden_do_jednego/01_Zadanie_1/exercise1.py
+++ 03_Dzien_02/02_Relacje_miedzy_tabelami_-_jeden_do_jednego/01_Zadanie_1/exercise1.py
@@ -1,4 +1,22 @@
-create_table_client_address = "Write the query here!"
-add_client_address_1 = "Write the query here!"
-add_client_address_2 = "Write the query here!"
-add_client_address_3 = "Write the query here!"
+create_table_client_address = """
+CREATE TABLE client_address(
+    id SERIAL, 
+    city VARCHAR(100), 
+    street VARCHAR(100), 
+    house_nr VARCHAR(32), 
+    client_id INT, PRIMARY KEY(id), 
+    FOREIGN KEY(client_id) REFERENCES clients(id)
+);
+"""
+
+add_client_address_1 = """
+INSERT INTO client_address(city, street, house_nr, client_id) VALUES ('Warsaw', 'Kwacza', 20, 5);
+"""
+
+add_client_address_2 = """
+INSERT INTO client_address(city, street, house_nr, client_id) VALUES ('Cracow', 'Myszy', 40, 6);
+"""
+
+add_client_address_3 = """
+INSERT INTO client_address(city, street, house_nr, client_id) VALUES ('Unknown', 'Anonymous', 0, 8);
+"""
