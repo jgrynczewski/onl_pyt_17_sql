9e69be64d55b62eeee972e633b5e8345adc1acd1
jgrynczewski <jgrynczewski@gmail.com>
2024-01-25 14:24:04 +0100

    Zad 2 (Relacje miedzy tabelami - wiele do wielu)

--- 03_Dzien_02/03_Relacje_miedzy_tabelami_-_wiele_do_wielu/02_Zadanie_2/exercise2.py
+++ 03_Dzien_02/03_Relacje_miedzy_tabelami_-_wiele_do_wielu/02_Zadanie_2/exercise2.py
@@ -1,4 +1,20 @@
-create_screenings_table = "Write the query here!"
-add_screenieng_1 = "Write the query here!"
-add_screenieng_2 = "Write the query here!"
+create_screenings_table = """
+CREATE TABLE screening(
+    id SERIAL PRIMARY KEY, 
+    datetime TIMESTAMP, 
+    cinema_id INT, 
+    movie_id INT, 
+    FOREIGN KEY(cinema_id) REFERENCES cinemas(id), 
+    FOREIGN KEY(movie_id) REFERENCES movies(id)
+);
+"""
+
+add_screenieng_1 = """
+INSERT INTO screening(cinema_id, movie_id, datetime) VALUES (1, 2, '2024-02-12 12:35:00');
+"""
+
+add_screenieng_2 = """
+INSERT INTO screening(cinema_id, movie_id, datetime) VALUES (3, 2, '2021-12-12 23:12:22');
+"""
+
 add_screenieng_3 = "Write the query here!"
